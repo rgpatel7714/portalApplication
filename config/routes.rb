026cf2f733e34250cfb23a1dcc2262d517343a85
rgp7714 <rgp7714@gmail.com>
2023-12-17 18:31:46 +0530

    Added devise for student and admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  
+  devise_for :students, path: 'students'
+  devise_for :admins, path: 'admins'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
