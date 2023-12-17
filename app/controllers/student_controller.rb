5db273aba0f806e1e33eaabed965cc9e6acb0f9b
rgp7714 <rgp7714@gmail.com>
2023-12-17 19:06:17 +0530

    Created log-in and log-out for students

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,8 @@
+class StudentController < ApplicationController
+  before_action :authenticate_student!
+  
+  def show
+  end
+  
+  end
+    
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :students, path: 'students'
-  devise_for :admins, path: 'admins'
+  
+  devise_for :students, controllers: { registrations: 'students/registrations',  sessions: 'students/sessions' }
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  
+  devise_scope :student do
+    root to: "students/sessions#new"
+  end
+  resources :student
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
