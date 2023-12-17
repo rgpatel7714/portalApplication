8f96f47f0ec4997c15a938985e239a61cf24540d
rgp7714 <rgp7714@gmail.com>
2023-12-17 19:51:53 +0530

    created log in and log out for admin

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     root to: "students/sessions#new"
   end
   resources :student
+  resources :admin
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
